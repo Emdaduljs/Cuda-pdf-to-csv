3988c2be8f260d1ea5fb5854ed5ad66c25169541
Emdaduljs <emdaduljs@gmail.com>
2025-07-30 23:24:29 +0600

    Delete cuda_pdf_to_csv_streamlit directory

--- cuda_pdf_to_csv_streamlit/pdf_to_csv/converter.py
+++
@@ -1,7 +0,0 @@
-from pytesseract import image_to_string
-
-def extract_text_from_area(image, coords):
-    left, top, right, bottom = map(int, coords)
-    cropped = image.crop((left, top, right, bottom))
-    text = image_to_string(cropped)
-    return text
