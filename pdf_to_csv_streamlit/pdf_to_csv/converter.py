84449ca999b0559e63ace0eb3a1ca6d37d807207
Emdaduljs <emdaduljs@gmail.com>
2025-07-30 23:25:03 +0600

    Delete pdf_to_csv_streamlit directory

--- pdf_to_csv_streamlit/pdf_to_csv/converter.py
+++
@@ -1,8 +0,0 @@
-from pytesseract import image_to_string
-from PIL import Image
-
-def extract_text_from_area(image, coords):
-    left, top, right, bottom = map(int, coords)
-    cropped = image.crop((left, top, right, bottom))
-    text = image_to_string(cropped)
-    return text
